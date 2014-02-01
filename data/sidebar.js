c8fd87bb94288ed81ba1c5c1f20027f5b5d1a57c
decipher <rodneyd.teal@gmail.com>
2014-02-01 01:37:18 -0600

    Update Sidebar

--- data/sidebar.js
+++ data/sidebar.js
@@ -1,6 +1,28 @@
-addon.port.emit("ready");
-addon.port.on("init", sayStuff);
+window.addEventListener('load', event => {
+    // Select source on click.
+    const source = document.querySelector('#input-source');
+    source.addEventListener('focus', event => source.select());
+    // Drag and drop source
+    window.addEventListener('dragenter', event => event.preventDefault());
+    window.addEventListener('dragover', event => event.preventDefault());
+    window.addEventListener('drop', event =>
+            addon.port.emit('get-type',
+            source.value = event.dataTransfer.getData('text/x-moz-url')));
 
-function sayStuff(message) {
-  console.log("got the message '" + message + "'" );
-}
+    // Single or multiple file torrent mode selection
+    const multiMode = document.querySelector('#multi-mode');
+    const singleMode = document.querySelector('#single-mode');
+    singleMode.checked = true;
+    multiMode.addEventListener('click', event => singleMode.checked = false);
+    singleMode.addEventListener('click', event => multiMode.checked = false);
+    const getMode = () => multiMode.checked;
+
+    addon.port.on('source', fileURL => source.value = fileURL);
+    addon.port.on('set-type', isDirectory => {
+        singleMode.checked = !isDirectory;
+        multiMode.checked = isDirectory;
+    });
+
+    document.querySelector('#select-source').addEventListener('click', event =>
+            addon.port.emit('select-source', getMode()));
+});
